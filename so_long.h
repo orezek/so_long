ca21ecf627e2340eac11e7ad5b69d97ce2bcb55b
O.R <orezek@seznam.cz>
2023-12-24 22:50:02 +0100

    Update: add game_map struct and typedef

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 19:27:03 by aldokezer         #+#    #+#             */
-/*   Updated: 2023/12/13 22:38:03 by orezek           ###   ########.fr       */
+/*   Updated: 2023/12/24 21:26:15 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,18 @@
 # define SO_LONG_H
 # include "libft/libft.h"
 # include "mlx42/include/MLX42/MLX42.h"
+
+typedef struct game_assets_s
+{
+	mlx_image_t wall;
+	mlx_image_t player;
+	mlx_image_t	collectible;
+	mlx_image_t	exit;
+	mlx_image_t	space;
+
+} game_assets_t;
+
+char	**generate_map(void);
+void	print_map(char **map);
 #endif
+
