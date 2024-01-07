81914c806758161c9c762cbd9e884174db560bd2
O.R <orezek@seznam.cz>
2024-01-07 21:19:43 +0100

    Testing: dynamic resizing - clean maps.c afterwards

--- utils/map.c
+++ utils/map.c
@@ -6,7 +6,7 @@
 /*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/24 20:47:57 by orezek            #+#    #+#             */
-/*   Updated: 2024/01/07 18:16:40 by orezek           ###   ########.fr       */
+/*   Updated: 2024/01/07 21:08:21 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -115,8 +115,11 @@ t_elem_size	*ft_get_elem_size(t_map_size *map_size)
 	mlx_get_monitor_size(0, &display_size->width, &display_size->height);
 	// elem_size.width = (double) display_size->width / map_size->width;
 	// elem_size.height = (double) display_size->height / map_size->height;
-	elem_size->width =  1680/ map_size->width;
-	elem_size->height = 1050 / map_size->height;
+	// elem_size->width =  1680/ map_size->width;
+	// elem_size->height = 1050 / map_size->height;
+	elem_size->width =  1850/ map_size->width;
+	elem_size->height = 1136 / map_size->height;
+	printf("%f:%f\n", elem_size->width, elem_size->height);
 	free(display_size);
 	return (elem_size);
 }
