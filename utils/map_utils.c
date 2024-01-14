73e629e9a74acf1853c17cbce61ffc8f8d1ca1aa
O.R <orezek@seznam.cz>
2024-01-14 21:48:36 +0100

    Update: implement map checking functionality

--- utils/map_utils.c
+++ utils/map_utils.c
@@ -6,11 +6,24 @@
 /*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 16:46:40 by orezek            #+#    #+#             */
-/*   Updated: 2024/01/14 17:41:13 by orezek           ###   ########.fr       */
+/*   Updated: 2024/01/14 21:37:35 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
+
+/*
+Todo:
+1) Map check
+2) Restructuing
+3) Memory deallocation
+4) Norm
+END
+Maybe iimplement two features
+1) Display move count
+2) Add Gargamel
+*/
+
 
 /*
 Check:
@@ -32,11 +45,79 @@ I need some universal character count of the map. To compare between the orginal
 */
 // creates a duplicate array and moves one coordinate at a time and marks it, the starting position is the player position or the exit position
 // it will mark everthing except wall - it cannot pass the walls
-char	**ft_map_fload(char **map)
+
+/*
+How to use this?
+
+I load a map, then I get a player position? How, I have game_context. Use that!
+What then? I need a array duplicate to run the fload on. Should I keep a pointer in the game_context?
+Oh, yes. So I need function array dup
+*/
+
+// free game_context->map-
+// free game_context->map->flooded_map
+void	ft_array_dup(t_game_context *game_context)
 {
-	;
+	char	**map_dup;
+	char	**map;
+	size_t	counter;
+	map = game_context->map->original_map;
+
+	counter = 0;
+	while (!map[counter])
+		counter++;
+	map_dup = malloc(sizeof(char *) * counter + 1);
+	if(!map_dup)
+		return (NULL);
+	counter = 0;
+	while(!map[counter])
+		map_dup[counter] = ft_strdup(map[counter]);
+	map_dup[counter] = NULL;
+	game_context->map->flooded_map = map_dup;
 }
-// checks if the map has X bigger then Y
+
+void	ft_map_flood(t_game_context *game_context, size_t y, size_t x)
+{
+	char	**map_array = game_context->map;
+
+	if (map_array[y][x] == 'c' || map_array[y][x] == 'e'
+		|| map_array[y][x] == 'p' || map_array[y][x] == '1'
+		|| map_array[y][x] == '2')
+		return ;
+	if (map_array[y][x] == 'C')
+		map_array[y][x] = 'c';
+	else if (map_array[y][x] == 'P')
+		map_array[y][x] = 'p';
+	else if (map_array[y][x] == 'E')
+		map_array[y][x] = 'e';
+	else if (map_array[y][x] == '0')
+		map_array[y][x] = '2';
+	ft_map_flood(map_array, game_context->player->player_position.y + 1, x);
+	ft_map_flood(map_array, y - 1, x);
+	ft_map_flood(map_array, y, x + 1);
+	ft_map_flood(map_array, y, x - 1);
+}
+
+void	map_flood_fill(char **map_array, size_t y, size_t x)
+{
+	if (map_array[y][x] == 'c' || map_array[y][x] == 'e'
+		|| map_array[y][x] == 'p' || map_array[y][x] == '1'
+		|| map_array[y][x] == '2')
+		return ;
+	if (map_array[y][x] == 'C')
+		map_array[y][x] = 'c';
+	else if (map_array[y][x] == 'P')
+		map_array[y][x] = 'p';
+	else if (map_array[y][x] == 'E')
+		map_array[y][x] = 'e';
+	else if (map_array[y][x] == '0')
+		map_array[y][x] = '2';
+	map_flood_fill(map_array, y + 1, x);
+	map_flood_fill(map_array, y - 1, x);
+	map_flood_fill(map_array, y, x + 1);
+	map_flood_fill(map_array, y, x - 1);
+}
+// checks if the map has X bigger then Y - look at Martin's solution
 int32_t	ft_is_map_rectangular(char **map)
 {
 	;
