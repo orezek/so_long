0ef9e5806c6d7c2b1e88c89de89f0acf0be4c2fb
O.R <orezek@seznam.cz>
2024-01-18 15:33:04 +0100

    Update: refactor src/utils.c to comply with the norm

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 17:31:52 by orezek            #+#    #+#             */
-/*   Updated: 2024/01/18 15:14:55 by orezek           ###   ########.fr       */
+/*   Updated: 2024/01/18 15:32:33 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,21 +28,21 @@ int	ft_free_array(char **array)
 	return (0);
 }
 
-
 void	*ft_duplicate_map(t_game_context *game_context)
 {
 	char	**map_dup;
 	char	**map;
 	size_t	counter;
+
 	map = game_context->map->original_map;
 	counter = 0;
 	while (map[counter] != NULL)
 		counter++;
 	map_dup = malloc(sizeof(char *) * (counter + 1));
-	if(!map_dup)
+	if (!map_dup)
 		return (NULL);
 	counter = 0;
-	while(map[counter] != NULL)
+	while (map[counter] != NULL)
 	{
 		map_dup[counter] = ft_strdup(map[counter]);
 		counter++;
@@ -58,7 +58,7 @@ void	ft_print_map(char **map)
 	tmp_ptr = map;
 	while (*tmp_ptr)
 	{
-		while(**tmp_ptr)
+		while (**tmp_ptr)
 		{
 			ft_printf("%c", **tmp_ptr);
 			(*tmp_ptr)++;
