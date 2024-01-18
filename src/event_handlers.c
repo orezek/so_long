cd900fcf30161cb533c77400e0fdbb3cf2f9fc22
O.R <orezek@seznam.cz>
2024-01-18 19:49:28 +0100

    Update: refactor even_handlers

--- src/event_handlers.c
+++ src/event_handlers.c
@@ -6,7 +6,7 @@
 /*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 18:59:47 by orezek            #+#    #+#             */
-/*   Updated: 2024/01/18 09:56:25 by orezek           ###   ########.fr       */
+/*   Updated: 2024/01/18 19:48:17 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,16 @@ void	on_esc_press(mlx_key_data_t k_data, void *param)
 	}
 }
 
+void	ft_handle_key_press(t_game_context *game)
+{
+	// game->game_images->player->instances[0].y -= (game->game_dimensions->element_size->height);
+	// game->player->player_position->y -= 1;
+	game->player->player_moves++;
+	ft_printf("Player move: %d\n", game->player->player_moves);
+	ft_check_collectibles(game);
+	ft_end_game(game);
+}
+
 void	on_wsad_press(mlx_key_data_t k_data, void *param)
 {
 	t_game_context	*game_context;
@@ -31,39 +41,46 @@ void	on_wsad_press(mlx_key_data_t k_data, void *param)
 	game_context = (t_game_context *) param;
 	if (k_data.key == MLX_KEY_W && k_data.action == MLX_PRESS && game_context->map->original_map[game_context->player->player_position->y -1][game_context->player->player_position->x] != '1')
 	{
+
 		game_context->game_images->player->instances[0].y -= (game_context->game_dimensions->element_size->height);
 		game_context->player->player_position->y -= 1;
-		game_context->player->player_moves++;
-		ft_printf("Player move: %d\n", game_context->player->player_moves);
-		ft_check_collectibles(game_context);
-		ft_end_game(game_context);
+		ft_handle_key_press(game_context);
+		// game_context->player->player_moves++;
+		// ft_printf("Player move: %d\n", game_context->player->player_moves);
+		// ft_check_collectibles(game_context);
+		// ft_end_game(game_context);
 	}
 	else if (k_data.key == MLX_KEY_S && k_data.action == MLX_PRESS && game_context->map->original_map[game_context->player->player_position->y + 1][game_context->player->player_position->x] != '1')
 	{
 		game_context->game_images->player->instances[0].y += (game_context->game_dimensions->element_size->height);
 		game_context->player->player_position->y += 1;
-		game_context->player->player_moves++;
-		ft_printf("Player move: %d\n", game_context->player->player_moves);
-		ft_check_collectibles(game_context);
-		ft_end_game(game_context);
+		ft_handle_key_press(game_context);
+		// game_context->player->player_moves++;
+		// ft_printf("Player move: %d\n", game_context->player->player_moves);
+		// ft_check_collectibles(game_context);
+		// ft_end_game(game_context);
 	}
 	else if (k_data.key == MLX_KEY_A && k_data.action == MLX_PRESS && game_context->map->original_map[game_context->player->player_position->y][game_context->player->player_position->x - 1] != '1')
 	{
 		game_context->game_images->player->instances[0].x -= (game_context->game_dimensions->element_size->width);
 		game_context->player->player_position->x -= 1;
-		game_context->player->player_moves++;
-		ft_printf("Player move: %d\n", game_context->player->player_moves);
-		ft_check_collectibles(game_context);
-		ft_end_game(game_context);
+		ft_handle_key_press(game_context);
+
+		// game_context->player->player_moves++;
+		// ft_printf("Player move: %d\n", game_context->player->player_moves);
+		// ft_check_collectibles(game_context);
+		// ft_end_game(game_context);
 	}
 	else if (k_data.key == MLX_KEY_D && k_data.action == MLX_PRESS && game_context->map->original_map[game_context->player->player_position->y][game_context->player->player_position->x + 1] != '1')
 	{
 		game_context->game_images->player->instances[0].x += (game_context->game_dimensions->element_size->width);
 		game_context->player->player_position->x += 1;
-		game_context->player->player_moves++;
-		ft_printf("Player move: %d\n", game_context->player->player_moves);
-		ft_check_collectibles(game_context);
-		ft_end_game(game_context);
+		ft_handle_key_press(game_context);
+
+		// game_context->player->player_moves++;
+		// ft_printf("Player move: %d\n", game_context->player->player_moves);
+		// ft_check_collectibles(game_context);
+		// ft_end_game(game_context);
 	}
 }
 
