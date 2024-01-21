068446cc841d745ad00f976d8837519ae7672306
O.R <orezek@seznam.cz>
2024-01-21 09:35:14 +0100

    Update: refactor main c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 19:24:31 by aldokezer         #+#    #+#             */
-/*   Updated: 2024/01/21 08:52:55 by orezek           ###   ########.fr       */
+/*   Updated: 2024/01/21 09:30:32 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,22 @@ void	ft_game_init(t_game_context *game_context)
 	game_context->mlx = mlx;
 }
 
-// void	ft_map_init(int32)
+t_game_context	*allocate_mem(t_game_context *game)
+{
+	game = malloc(sizeof(t_game_context));
+	if (!game)
+		return (NULL);
+	game->map = malloc(sizeof(t_map));
+	if (!game->map)
+		return (free(game), NULL);
+	game->game_dimensions = malloc(sizeof(t_game_dimensions));
+	if (!game->game_dimensions)
+		return (free(game->map), free(game), NULL);
+	game->game_dimensions->display_size = malloc(sizeof(t_display_size));
+	if (!game->game_dimensions->display_size)
+		return (free(game->map), free(game), free(game->game_dimensions), NULL);
+	return (game);
+}
 
 int32_t	main(int32_t argc, const char *argv[])
 {
@@ -35,8 +50,7 @@ int32_t	main(int32_t argc, const char *argv[])
 	ft_check_program_arguments(argc, argv);
 	ft_check_file_name((char *)argv[1]);
 	map = ft_load_map((char *)argv[1]);
-	game_context = malloc(sizeof(t_game_context));
-	game_context->map = malloc(sizeof(t_map));
+	game_context = allocate_mem(game_context);
 	game_context->map->original_map = map;
 	ft_validate_map_dimensions(game_context->map->original_map);
 	ft_check_map_boundary(game_context->map->original_map);
@@ -46,8 +60,6 @@ int32_t	main(int32_t argc, const char *argv[])
 	ft_verify_game_map(game_context->map->original_map, game_context->map->flooded_map);
 	game_context->player->player_moves = 0;
 	game_context->exit_position = ft_get_exit_position(game_context->map->original_map);
-	game_context->game_dimensions = malloc(sizeof(t_game_dimensions));
-	game_context->game_dimensions->display_size = malloc(sizeof(t_display_size));
 	ft_get_map_size(game_context);
 	ft_count_collectibles(game_context);
 	ft_game_init(game_context);
